b4346bab9bbc0f12649a17848511ce01fc3c0d67
Alfredo Dheryk <dheryk@gmail.com>
2024-11-08 18:35:22 -0300

    UpdateusuariosController

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -14,7 +14,7 @@ exports.createUsuario = async (req, res) => {
   
   console.log("Dados recebidos para criação:", req.body); // Para debugar o payload
 
-  const { data, error } = await supabase
+  const {error} = await supabase
     .from('usuarios')
     .insert([{ nome, email, nome_usuario, senha, cpf, funcao, obra_id }]);
 
@@ -23,13 +23,8 @@ exports.createUsuario = async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 
-  // Confirmação de que o usuário foi realmente criado
-  if (!data || data.length === 0) {
-    return res.status(400).json({ error: 'Erro ao criar usuário no banco de dados' });
-  }
-
-  console.log('Usuário criado com sucesso:', data[0]);
-  return res.status(201).json(data[0]); // Retorna o usuário criado com status 201
+  console.log('Usuário criado com sucesso:');
+  return res.status(201).json({message:'Usuário criado com sucesso:'}); // Retorna o usuário criado com status 201
 };
 
 // Editar usuário
@@ -37,34 +32,27 @@ exports.updateUsuario = async (req, res) => {
   const { id } = req.params; // ID do usuário a ser atualizado
   const { nome, email, nome_usuario, senha, cpf, funcao, obra_id } = req.body;
 
-  const { data, error } = await supabase
+  const {error} = await supabase
     .from('usuarios')
     .update({ nome, email, nome_usuario, senha, cpf, funcao, obra_id })
     .eq('id', id);
 
   if (error) return res.status(500).json({ error: error.message });
 
-  if (!data || data.length === 0) {
-    return res.status(404).json({ error: 'Usuário não encontrado para atualizar' });
-  }
-
-  res.status(200).json(data[0]);
+ 
+  res.status(200).json({message:'Usuário atualizado com sucesso:'});
 };
 
 // Deletar usuário
 exports.deleteUsuario = async (req, res) => {
   const { id } = req.params; // ID do usuário a ser deletado
   
-  const { data, error } = await supabase
+  const {error} = await supabase
     .from('usuarios')
     .delete()
     .eq('id', id);
 
   if (error) return res.status(500).json({ error: error.message });
-
-  if (!data || data.length === 0) {
-    return res.status(404).json({ error: 'Usuário não encontrado para deletar' });
-  }
 
   res.status(200).json({ message: 'Usuário deletado com sucesso!' });
 };
